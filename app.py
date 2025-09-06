4c2a771b264e2909812be7d4c8a7a77607584056
goatman88 <mtw2700@gmail.com>
2025-09-06 12:39:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,14 @@ logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.environ.get("API_TOKEN", "").strip()
 
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 # -----------------------------------------------------------------------------
 # Helpers
 # -----------------------------------------------------------------------------
