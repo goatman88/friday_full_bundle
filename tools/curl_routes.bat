13f1008cd4c9ff7f87cee61e74456482cc65dda9
goatman88 <mtw2700@gmail.com>
2025-09-12 03:34:30 -0500

    Update curl_routes.bat

--- tools/curl_routes.bat
+++ tools/curl_routes.bat
@@ -1,14 +1,15 @@
 @echo off
-REM Requires curl.exe (built into Windows 10+)
+REM Usage:
+REM   set FRIDAY_BASE=https://friday-099e.onrender.com
+REM   set API_TOKEN=mvtest_7hj3w8k2
+REM   tools\curl_routes.bat
 
-IF "%FRIDAY_BASE%"==""  echo Set FRIDAY_BASE first & exit /b 1
-IF "%API_TOKEN%"==""    echo Set API_TOKEN first & exit /b 1
+if "%FRIDAY_BASE%"=="" echo Set FRIDAY_BASE first & exit /b 1
+if "%API_TOKEN%"==""   echo Set API_TOKEN first & exit /b 1
 
-curl -s ^
+curl -s "%FRIDAY_BASE%/__routes" ^
   -H "Authorization: Bearer %API_TOKEN%" ^
-  -H "Accept: application/json" ^
-  "%FRIDAY_BASE%/__routes"
-
+  -H "Accept: application/json"
 echo.
-pause
+
 
