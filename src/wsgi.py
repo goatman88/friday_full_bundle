e29ddd3479f6b2d91a5c7972dc222696c9c0fdb4
goatman88 <mtw2700@gmail.com>
2025-09-15 02:07:54 -0500

    Update wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,8 +1,7 @@
-# Minimal, import from module in the *current* folder (because we cd into src)
-from app import app  # <- DO NOT prefix with "src."
+from app import app
 
-# WSGI entry point object that Waitress will look for
-# (Waitress accepts "wsgi:app" so this must be named "app")
-# Nothing else needed here.
+if __name__ == "__main__":
+    app.run()
+
 
 
