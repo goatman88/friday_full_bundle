fe0778ddab9ec02bcb9e8a55eae1c02100336459
goatman88 <mtw2700@gmail.com>
2025-09-14 19:19:09 -0500

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+# src/wsgi.py
+# Force-load our guard before importing the app
+import sitecustomize  # noqa: F401
+
+# Now import your Flask app
+from app import app  # this must define `app = Flask(__name__)`
