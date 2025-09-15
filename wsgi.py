7222fea5a1676290870a9d9828ddee314480a4f1
goatman88 <mtw2700@gmail.com>
2025-09-14 21:55:55 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,6 @@ import sitecustomize  # noqa: F401
 
 # Now import your Flask app
 from app import app  # this must define `app = Flask(__name__)`
+from app import app
+if __name__ == "__main__":
+    app.run()
