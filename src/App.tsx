f09be0b3b20264856894e391fe12b02306b0786e
goatman88 <mtw2700@gmail.com>
2025-09-16 01:34:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,3 +11,16 @@ export default function App() {
   );
 }
 
+"use client";
+import CrawlUploadWithStatus from "@/components/CrawlUploadWithStatus";
+import MultiUploaderWithStatus from "@/components/MultiUploaderWithStatus";
+
+export default function Page() {
+  return (
+    <main style={{ padding: 20 }}>
+      <CrawlUploadWithStatus />
+      <hr />
+      <MultiUploaderWithStatus />
+    </main>
+  );
+}
