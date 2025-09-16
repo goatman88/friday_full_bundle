2740a028f0717c73598a8c9ed3e183dbbbec36c5
goatman88 <mtw2700@gmail.com>
2025-09-16 12:43:05 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
-import App from './pages/App.jsx'
-import MultiUploader from './pages/MultiUploader.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/multi-uploader', element: <MultiUploader /> },
-])
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
+
