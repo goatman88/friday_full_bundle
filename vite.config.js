8490f082d5af20c00d03468733136bd141fb55f8
goatman88 <mtw2700@gmail.com>
2025-09-18 18:13:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: process.env.VITE_API_BASE
-      ? undefined
-      : {
-          '/api': {
-            target: 'http://localhost:8000', // change if you run a local backend
-            changeOrigin: true,
-          }
-        }
-  }
-})
+});
+
