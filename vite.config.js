efc0ca777e87317ebbb7cb27b6cf00a11eb9ed35
goatman88 <mtw2700@gmail.com>
2025-09-20 00:55:30 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// friday-frontend/vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  preview: { host: "0.0.0.0", port: 5173 },
+  build: { outDir: "dist" },
+  // Passing an object prevents Vite from searching for external PostCSS config
+  css: { postcss: { plugins: [] } },
 });
 
