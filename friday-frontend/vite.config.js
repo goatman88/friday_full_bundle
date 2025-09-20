1e728d5b523f59029a2d16ac3c6765ec594a071b
goatman88 <mtw2700@gmail.com>
2025-09-20 13:24:06 -0500

    Update vite.config.js

--- friday-frontend/vite.config.js
+++ friday-frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Minimal, no custom "define", no PostCSS config needed
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "dist" },
+  build: { outDir: "dist", sourcemap: false }
 });
+
 
 
