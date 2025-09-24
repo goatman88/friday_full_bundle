89db8391623bfad8447fa420d636f2c0886e9856
goatman88 <mtw2700@gmail.com>
2025-09-23 20:05:58 -0500

    Create run-env.ps1

---
+++ scripts/run-env.ps1
@@ -0,0 +1,4 @@
+# scripts/run-env.ps1
+Write-Host "Running env.ps1 to set local environment..." -ForegroundColor Cyan
+.\scripts\env.ps1
+Write-Host "Environment variables loaded." -ForegroundColor Green
