673d0d19902a1ea8c7d02ee494e510c7df47917d
goatman88 <mtw2700@gmail.com>
2025-09-24 15:09:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
+
 export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true
+      "/api": {
+        target: process.env.VITE_BACKEND_BASE || "http://localhost:8000",
+        changeOrigin: true,
+      },
+      "/health": {
+        target: process.env.VITE_BACKEND_BASE || "http://localhost:8000",
+        changeOrigin: true,
       }
     }
   }
 })
+
 
 
 
